30fd5a4cacb3062b5c691c6ae92d45f9b3f3eb27
nuri <noori1050@naver.com>
2022-04-02 00:01:34 +0900

    [Add] 사장 유저 MVP 스키마 구현

--- back/db/models/index.js
+++ back/db/models/index.js
@@ -1,5 +1,6 @@
-import Sequelize from 'sequelize';
-import config from '../config/config';
+import Sequelize from "sequelize";
+import config from "../config/config";
+import BossUser from "./schemas/bossUser";
 console.log(config);
 
 let sequelize = new Sequelize(
@@ -13,5 +14,9 @@ const db = {};
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
+
+db.BossUser = BossUser;
+BossUser.init(sequelize);
+BossUser.associate(db);
 
 export default db;
