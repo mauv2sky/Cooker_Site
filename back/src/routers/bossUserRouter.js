3a13792a6033e51bc87ae16ed2a2d5f61f494556
nuri <noori1050@naver.com>
2022-06-29 02:00:15 +0900

    [Docs] bossUser swagger 기능 구현 완료
    - get boss/{ceo_id}
    - put boss/{id}
    - delete boss/{id}

--- back/src/routers/bossUserRouter.js
+++ back/src/routers/bossUserRouter.js
@@ -64,6 +64,30 @@ bossUserAuthRouter.post('/boss/join', bossUserController.register);
  */
 bossUserAuthRouter.post('/boss/login', bossUserController.login);
 
+/**
+ * @swagger
+ * /boss/{ceo_id}:
+ *  get:
+ *    tags:
+ *      - bossUser
+ *    description: bossUer 유저 정보 가져오기
+ *    security:
+ *      - bearerAuth: []
+ *    parameters:
+ *      - name: ceo_id
+ *        in: path
+ *        required: true
+ *        schema:
+ *          type: string
+ *    responses:
+ *      200:
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: "#/components/schemas/BossUser"
+ *        description: bossUser 현재 유저 정보 가져오기 성공
+ */
+// TODO: parameter로 ceo_id 받기 대신 token에서 추출하기
 // 사장 유저 정보 가져오기
 bossUserAuthRouter.get(
   '/boss/:ceo_id',
@@ -71,12 +95,78 @@ bossUserAuthRouter.get(
   bossUserController.findByCeoId
 );
 
+/**
+ * @swagger
+ * /boss/{id}:
+ *  put:
+ *    tags:
+ *      - bossUser
+ *    description: bossUer 유저 정보 수정하기
+ *    security:
+ *      - bearerAuth: []
+ *    parameters:
+ *      - name: id
+ *        in: path
+ *        required: true
+ *        schema:
+ *          type: number
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              email:
+ *                type: string
+ *              tel:
+ *                type: string
+ *              description:
+ *                type: string
+ *              img:
+ *                type: string
+ *    responses:
+ *      200:
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: "#/components/schemas/BossUser"
+ *        description: bossUser 현재 유저 정보 수정하기 성공
+ */
 // 사용자 정보 수정하기
 bossUserAuthRouter.put(
   '/boss/:id',
   login_required,
   bossUserController.updateBossUserInfo
 );
+
+/**
+ * @swagger
+ * /boss/{id}:
+ *  delete:
+ *    tags:
+ *      - bossUser
+ *    description: bossUer 유저 탈퇴하기
+ *    security:
+ *      - bearerAuth: []
+ *    parameters:
+ *      - name: id
+ *        in: path
+ *        required: true
+ *        schema:
+ *          type: number
+ *    responses:
+ *      200:
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                ok:
+ *                  type: boolean
+ *                  default: true
+ *        description: bossUser 유저 탈퇴하기 성공
+ */
 // 사장 정보 삭제하기(탈퇴)
 bossUserAuthRouter.delete(
   '/boss/:id',
